2fdb74d8a6a5182f3e6d0c0e2e8cf77cbfecd229
XueMei-L <alu0101225845@ull.edu.es>
2022-04-07 11:58:32 +0200

    factory method terminado

--- tests/modificacion/eje1.spec.ts
+++ tests/modificacion/eje1.spec.ts
@@ -1,25 +1,30 @@
 import 'mocha';
 import { expect } from 'chai';
-import { isConvertible } from "../../src/isConvertible";
-import { ImperialLength } from "../../src/ImperialLength";
+import { ConcreteCreatorFuego, ConcreteCreatorElectri } from "../../src/eje1";
+import { clientCode } from "../../src/eje1";
 
 
 
 describe('Tests for the modification', () => {
-    //Longuitud
-    enum unidadLonguitud { 
-        m  = Math.pow(10, 0),
-        dm = Math.pow(10, -1),
-        cm = Math.pow(10, -2),
-        mm = Math.pow(10, -3),
-    }
-    
-    const longuitudAImperial = new ImperialLength(unidadLonguitud.mm, 500);
-    // protected unidad1:number, protected unidMag:unidadLonguitud)
-    describe('Longuitud', () => {
-        it('7 dm = 70 cm', () => {
-            expect(longuitudAImperial.convertir(unidadLonguitud.cm)).to.be.equal(70);
-        });
+
+    const fuegoCharizard = new ConcreteCreatorFuego("charizard", 60, 55, "fire", [84, 78, 100, 78]);
+    const electricPikachu = new ConcreteCreatorElectri("pikachu", 50, 45, "electric", [90, 55, 110, 60]);
+
+    it('Charizard', () => {
+        expect(fuegoCharizard).not.to.be.null;
     });
+
+    it('pikachu', () => {
+        expect(electricPikachu).not.to.be.null;
+    });
+
+
+    // it('Charizard', () => {
+    //     expect(clientCode(fuegoCharizard)).to.eq("");
+    // });
+
+    // it('Pikachu', () => {
+    //     expect(clientCode(electricPikachu)).to.eq("");
+    // });
 
 });
